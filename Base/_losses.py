34e4ab820486570cface91af29b264033b2e61e9
samanemami <saman.emami@hotmail.com>
2023-02-14 14:12:57 +0100

    modified:   .gitignore 	modified:   Base/__init__.py 	modified:   Base/_base.py 	modified:   Base/_losses.py 	deleted:    Example/__init__.py 	deleted:    Example/model_training.py 	deleted:    model/__init__.py 	new file:   models/__init__.py 	new file:   models/deepgbnn.py 	renamed:    model/gbcnn.py -> models/gbcnn.py 	modified:   requirements/runtime.txt

--- Base/_losses.py
+++ Base/_losses.py
@@ -1,4 +1,4 @@
-""" Loss functions for Gradient Boosted Convolutional Neural Network """
+""" Loss functions for Deep Gradient Boosted Neural Networks """
 
 # Author: Seyedsaman Emami
 # Author: Gonzalo Martínez-Muñoz
@@ -6,7 +6,6 @@
 # Licence: GNU Lesser General Public License v2.1 (LGPL-2.1)
 
 import numpy as np
-
 from abc import abstractmethod
 from scipy.special import logsumexp
 
@@ -31,12 +30,51 @@ class loss:
         """call"""
 
 
+class squared_loss(loss):
+    """ Squared loss for regression problems """
+
+    def model0(self, y):
+        return np.ones(1)*np.mean(y)
+
+    def derive(self, y, prev):
+        return y-prev
+
+    def newton_step(self, y, residuals, new_predictions):
+        return 1
+
+    def __call__(self, y, pred):
+        return (y-pred)**2
+
+
 class classification_loss(loss):
     """ Base class for classification losses """
 
     @abstractmethod
     def raw_predictions_to_probs(self, preds):
         """abstract method for raw_predictions_to_probs"""
+
+
+class log_exponential_loss(classification_loss):
+    """ Log-exponential loss for binary classification tasks """
+
+    def model0(self, y):
+        ymed = np.mean(y)
+        return np.ones(1)*(0.5 * np.log((1+ymed) / (1-ymed)))
+
+    def derive(self, y, prev):
+        return np.nan_to_num(2.0 * y / (1 + np.exp(2.0 * y * prev)))
+
+    def newton_step(self, y, residuals, new_predictions):
+        f_m = np.squeeze(new_predictions)
+        return np.sum(residuals * f_m) / np.sum(residuals * f_m * f_m * (2.0 * y - residuals))
+
+    def raw_predictions_to_probs(self, preds):
+        preds = np.squeeze(1 / (1 + np.exp(-2 * preds)))
+        return np.vstack((1-preds, preds)).T
+
+    def __call__(self, y, pred):
+        return np.log(1 + np.exp(-2.0 * y * pred))
+
 
 class multi_class_loss(classification_loss):
     """ Entropy loss por multi-class classification tasks """
---
+++ models/__init__.py
@@ -0,0 +1,4 @@
+from .gbcnn import GBCNN
+from .deepgbnn import DeepClassifier, DeepRegressor
+
+__all__ = ["GBCNN", "DeepClassifier", "DeepRegressor"]
