2e15d7b6723c0345b4a9beafee50a6eb62945120
Saman .E <54855771+samanemami@users.noreply.github.com>
2023-03-13 17:12:09 +0100

    Update __init__.py

--- Base/__init__.py
+++ Base/__init__.py
@@ -1,10 +1,9 @@
 from ._cnn import _layers
 from ._params import Params
 from ._base import BaseGBCNN, BaseGBDNN
-from ._losses import multi_class_loss, squared_loss, log_exponential_loss
+from ._losses import multi_class_loss, log_exponential_loss
 
 __all__ = ["Params",
-           "squared_loss",
            "log_exponential_loss",
            "multi_class_loss",
            "BaseGBCNN",
